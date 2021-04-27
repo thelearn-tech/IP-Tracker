e5b54a27277ffe10aa2eb258e9bd0d790c052f7b
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2021-04-27 20:57:17 +0530

    Add files via upload

---
+++ iptracker.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+banner() {
+	echo -e """
+  \033[1;32m###################################
+  ##########   \033[1;36mIP Tracker  \033[1;32m##########
+  ##########    \033[1;36mv1.0       \033[1;32m##########
+  ######### \033[38;5;214mby thelearn-tech \033[1;32m########
+  ###################################
+	"""
+}
+
+myipaddr() {
+
+myipaddripapico=$(curl -s "https://ipapi.co//json" -L)
+myipaddripapicom=$(curl -s "http://ip-api.com/json/" -L)
+myip=$(echo $myipaddripapico | grep -Po '(?<="ip":)[^,]*' | tr -d '[]"')
+mycity=$(echo $myipaddripapico | grep -Po '(?<="city":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+myregion=$(echo $myipaddripapico | grep -Po '(?<="region":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mycountry=$(echo $myipaddripapico | grep -Po '(?<="country_name":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mylat=$(echo $myipaddripapicom | grep -Po '(?<="lat":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mylon=$(echo $myipaddripapicom | grep -Po '(?<="lon":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mytime=$(echo $myipaddripapicom | grep -Po '(?<="timezone":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mypostal=$(echo $myipaddripapicom | grep -Po '(?<="zip":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+myisp=$(echo $myipaddripapico | grep -Po '(?<="org":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+myasn=$(echo $myipaddripapico | grep -Po '(?<="asn":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mycountrycode=$(echo $myipaddripapico | grep -Po '(?<="country_code":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mycurrency=$(echo $myipaddripapico | grep -Po '(?<="currency":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mylanguage=$(echo $myipaddripapico | grep -Po '(?<="languages":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+mycalling=$(echo $myipaddripapico | grep -Po '(?<="country_calling_code":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+
+
+
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m Ip Address    \e[0m\e[1;96m:\e[0m\e[1;92m   $myip\e[0m\n"
+printf "  \e[0m\e[1;93m City          \e[0m\e[1;96m:\e[0m\e[1;92m   $mycity\e[0m\n"
+printf "  \e[0m\e[1;93m Region        \e[0m\e[1;96m:\e[0m\e[1;92m   $myregion\e[0m\n"
+printf "  \e[0m\e[1;93m Country       \e[0m\e[1;96m:\e[0m\e[1;92m   $mycountry\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m Latitude      \e[0m\e[1;96m:\e[0m\e[1;92m    $mylat\e[0m\n"
+printf "  \e[0m\e[1;93m Longitude     \e[0m\e[1;96m:\e[0m\e[1;92m    $mylon\e[0m\n"
+printf "  \e[0m\e[1;93m Time Zone     \e[0m\e[1;96m:\e[0m\e[1;92m    $mytime\e[0m\n"
+printf "  \e[0m\e[1;93m Postal Code   \e[0m\e[1;96m:\e[0m\e[1;92m    $mypostal\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m ISP           \e[0m\e[1;96m:\e[0m\e[1;92m   $myisp\e[0m\n"
+printf "  \e[0m\e[1;93m ASN           \e[0m\e[1;96m:\e[0m\e[1;92m   $myasn\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m Country Code  \e[0m\e[1;96m:\e[0m\e[1;92m   $mycountrycode\e[0m\n"
+printf "  \e[0m\e[1;93m Currency      \e[0m\e[1;96m:\e[0m\e[1;92m   $mycurrency\e[0m\n"
+printf "  \e[0m\e[1;93m Languages     \e[0m\e[1;96m:\e[0m\e[1;92m   $mylanguage\e[0m\n"
+printf "  \e[0m\e[1;93m Calling Code  \e[0m\e[1;96m:\e[0m\e[1;92m   $mycalling\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m GOOGLE Maps   \e[0m\e[1;96m:\e[0m\e[1;94m  https://maps.google.com/?q=$mylat,$mylon\e[0m\n"
+sleep 1
+exit 1
+
+
+}
+
+useripaddr() {
+   echo no
+
+}
+
+help_text() {
+	echo -e """
+ \033[1;31mI am not Responsibale for any Illegal 
+ Use of this Program.
+ 
+ \033[1;32muse \033[1;33miptracker -m \033[1;32mto track my ip address
+ use \033[1;33miptracker -t <target ip> \033[1;32mto track target ip
+ """
+}
+ 
+case $1 in 
+-m)
+banner
+myipaddr
+;;
+-t)
+banner
+ipaddripapico=$(curl -s "https://ipapi.co/$2/json" -L)
+ipaddripapicom=$(curl -s "http://ip-api.com/json/$2" -L)
+userip=$(echo $ipaddripapico | grep -Po '(?<="ip":)[^,]*' | tr -d '[]"')
+usercity=$(echo $ipaddripapico | grep -Po '(?<="city":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+useregion=$(echo $ipaddripapico | grep -Po '(?<="region":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+usercountry=$(echo $ipaddripapico | grep -Po '(?<="country_name":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+userlat=$(echo $ipaddripapicom | grep -Po '(?<="lat":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+userlon=$(echo $ipaddripapicom | grep -Po '(?<="lon":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+usertime=$(echo $ipaddripapicom | grep -Po '(?<="timezone":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+userpostal=$(echo $ipaddripapicom | grep -Po '(?<="zip":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+userisp=$(echo $ipaddripapico | grep -Po '(?<="org":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+userasn=$(echo $ipaddripapico | grep -Po '(?<="asn":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+usercountrycode=$(echo $ipaddripapico | grep -Po '(?<="country_code":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+usercurrency=$(echo $ipaddripapico | grep -Po '(?<="currency":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+userlanguage=$(echo $ipaddripapico | grep -Po '(?<="languages":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+usercalling=$(echo $ipaddripapico | grep -Po '(?<="country_calling_code":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
+
+
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m Ip Address    \e[0m\e[1;96m:\e[0m\e[1;92m   $userip\e[0m\n"
+printf "  \e[0m\e[1;93m City          \e[0m\e[1;96m:\e[0m\e[1;92m   $usercity\e[0m\n"
+printf "  \e[0m\e[1;93m Region        \e[0m\e[1;96m:\e[0m\e[1;92m   $useregion\e[0m\n"
+printf "  \e[0m\e[1;93m Country       \e[0m\e[1;96m:\e[0m\e[1;92m   $usercountry\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m Latitude      \e[0m\e[1;96m:\e[0m\e[1;92m    $userlat\e[0m\n"
+printf "  \e[0m\e[1;93m Longitude     \e[0m\e[1;96m:\e[0m\e[1;92m    $userlon\e[0m\n"
+printf "  \e[0m\e[1;93m Time Zone     \e[0m\e[1;96m:\e[0m\e[1;92m    $usertime\e[0m\n"
+printf "  \e[0m\e[1;93m Postal Code   \e[0m\e[1;96m:\e[0m\e[1;92m    $userpostal\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m ISP           \e[0m\e[1;96m:\e[0m\e[1;92m   $userisp\e[0m\n"
+printf "  \e[0m\e[1;93m ASN           \e[0m\e[1;96m:\e[0m\e[1;92m   $userasn\e[0m\n"
+printf "\e[0m\n"
+
+printf "  \e[0m\e[1;93m Country Code  \e[0m\e[1;96m:\e[0m\e[1;92m   $usercountrycode\e[0m\n"
+printf "  \e[0m\e[1;93m Currency      \e[0m\e[1;96m:\e[0m\e[1;92m   $usercurrency\e[0m\n"
+printf "  \e[0m\e[1;93m Languages     \e[0m\e[1;96m:\e[0m\e[1;92m   $userlanguage\e[0m\n"
+printf "  \e[0m\e[1;93m Calling Code  \e[0m\e[1;96m:\e[0m\e[1;92m   $usercalling\e[0m\n"
+printf "\e[0m\n"
+printf "  \e[0m\e[1;93m GOOGLE Maps   \e[0m\e[1;96m:\e[0m\e[1;94m  https://maps.google.com/?q=$userlat,$userlon\e[0m\n"
+sleep 1
+exit 1
+;;
+--help)
+banner
+help_text
+;;
+*)
+echo ""
+echo -e "\033[1;31m Wrong input "
+echo -e " \033[1;32muse \033[1;33miptracker --help  " 
+echo ""
+;;
+esac 
